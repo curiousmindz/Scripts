cfed64952b09db6c141eb5d7aea4355fb8823db8
curiousmindz <sukhpreet.boparai@itweapons.com>
2015-02-23 21:15:57 -0500

    Create vmgetinfo.ps1

---
+++ vmgetinfo.ps1
@@ -0,0 +1,12 @@
+QUESTION 236 
+Your network contains an Active Directory domain named contoso.com. 
+The domain contains a server named Server1 that runs a Server Core installation of Windows Server 2012 R2. 
+You need to deploy a certification authority (CA) to Server1. 
+The CA must support the auto- enrollment of certificates. 
+Which two cmdlets should you run? (Each correct answer presents part of the solution. 
+Choose two.)
+A.    Add-CAAuthoritylnformationAccess 
+B.    Install-AdcsCertificationAuthority 
+C.    Add-WindowsFeature 
+D.    Install-AdcsOnlineResponder 
+E.    Install-AdcsWebEnrollment
